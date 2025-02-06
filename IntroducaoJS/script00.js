937f2e645d1404ad4212a71671d17434658f2d2d
Marialu97 <marialuizaelias04@gmail.com>
2025-02-06 11:32:54 -0300

    06.02.25 introdução Js

---
+++ IntroducaoJS/script00.js
@@ -0,0 +1,33 @@
+const texto = "Alo, Mundo!!!";
+var boasVindas = "Bem-Vindo, Maria de JS";
+boasVindas = "Boas Vindas Alunos";
+//texto = "fvecdbdv";
+
+console.log(texto);
+console.log(boasVindas)
+
+//Tipos de Dasdos
+
+let string = "Variavel de Texto"; 
+//Sempre entre Aspas duplas ou simples
+let numero1 = 42; // número inteiro
+let numero2 = 10.65; // número decimal
+let numero3 = "2"; // número em texto
+let boolean = true;
+let boolean2 = false;
+let boolean3 = "true"; // texto
+let undefined;
+let nula = null;
+
+//let, var, const
+let resultado = true; // atribuição
+resultado = false; // reatribui
+// let resultado = true; -proibido reescrever
+
+var nota = 10; // atribuição
+nota = 9; // reatribuição
+var nota = 8; // reescrita
+
+const fixo = 42;
+// fixe = 43 ; erro - não posso reatribuir
+// const fixo = 41 = erro - não posso rescrita 
---
+++ IntroducaoJS/script01Operacoes.js
@@ -0,0 +1,37 @@
+// Tipos de Operacões em JavaScript
+
+let a = 10
+let b = 3
+
+console.log("soma:")
+console.log(a+b); //13
+console.log("subtração:")
+console.log(a-b); //7
+console.log("Mutiplicação:")
+console.log(a*b); //30
+console.log("Divisão:")
+console.log(a/b); //3.33
+console.log("Resto da Divisão:")
+console.log(a%b); //1
+
+//Operadores Comparação 
+//(> , >= ,< ,<= , == ,===)
+console.log("Operadores Relacionais")
+console.log(5>10) //false
+console.log(10 == "10"); //true 
+console.log(10 === "10"); //false
+
+//Operadores Lógicos
+
+let nota1 = 5;
+let nota2 = 8;
+
+// our (||)
+console.log(nota1>=7 || nota2>=7); //true
+
+// and (&&)
+console.log(nota1>=7 && nota2 >=7); //false
+
+// not (!)
+console.log(!true); //false
+
